c5a590344ebccddc0c6656c1b920ff101a97b723
sotogito <108535526+sotogito@users.noreply.github.com>
2024-05-31 15:58:50 +0900

    feat : arraylist 기능 구현

--- src/collection_1/list/ArrayList_1.java
+++ src/collection_1/list/ArrayList_1.java
@@ -14,15 +14,28 @@ public class ArrayList_1 {
 
     private void main(){
         add();
+        addElementAt(0);
+
+        checkDuplication("춘심이");
+        checkDuplication("마루");
+        deleteElementAt(0);
+
         printElementAt(0);
         printAll();
+        printAllReverse();
     }
 
+    //리스트에 데이터 넣기
     private void add(){
         name.add("춘심이");
         name.add("꼬막이");
         name.add("구름이");
 
+    }
+
+    //특정 인덱스에 데이터 넣기
+    private void addElementAt(int index){
+        name.add(0,"코코");
     }
 
     //특정 요소 값만 출력
@@ -35,6 +48,26 @@ public class ArrayList_1 {
         for(String name : name){
             System.out.println(name);
         }
+    }
+
+    //전체 반대로 출력
+    private void printAllReverse(){
+        for(int i = name.size() - 1; i >= 0; i--){
+            System.out.println(name.get(i));
+        }
+    }
+
+
+    //중복되지 않는 값만 넣기
+    private void checkDuplication(String dogName){
+        if(!name.contains(dogName)){
+            name.add(dogName);
+        }
+    }
+
+    private void deleteElementAt(int index){
+        int lastIndex = name.size()-1;
+        name.remove(lastIndex);
     }
 
 
